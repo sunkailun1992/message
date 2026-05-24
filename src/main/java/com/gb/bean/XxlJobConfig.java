b75327d66b11ea93e86a0d45e8261db5c1898b9b
孙凯伦 <kl19921210@gmail.com>
2026-05-24 22:54:50 +0800

    完善

---
+++ src/main/java/com/gb/bean/XxlJobConfig.java
@@ -0,0 +1,51 @@
+package com.gb.bean;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * XXL-JOB executor configuration.
+ */
+@Slf4j
+@Configuration
+public class XxlJobConfig {
+
+    @Value("${xxl.job.admin.addresses:}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.accessToken:}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.appname:${spring.application.name:}}")
+    private String appname;
+
+    @Value("${xxl.job.executor.ip:}")
+    private String ip;
+
+    @Value("${xxl.job.executor.port:0}")
+    private int port;
+
+    @Value("${xxl.job.executor.logpath:./logs/xxl-job/jobhandler}")
+    private String logPath;
+
+    @Value("${xxl.job.executor.logretentiondays:30}")
+    private int logRetentionDays;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        log.info(">>>>>>>>>>> xxl-job config init, adminAddresses:{}, appname:{}:{}, ip:{}, port:{}",
+                adminAddresses, appname, ip, port);
+        XxlJobSpringExecutor executor = new XxlJobSpringExecutor();
+        executor.setAdminAddresses(adminAddresses);
+        executor.setAccessToken(accessToken);
+        executor.setAppname(appname);
+        executor.setIp(ip);
+        executor.setPort(port);
+        executor.setLogPath(logPath);
+        executor.setLogRetentionDays(logRetentionDays);
+        return executor;
+    }
+}
